6546ce3c4a398e4b249b8ee9bb4ff5e14c2bfec2
maobmo <monyorodemongare@gmail.com>
2024-07-19 09:41:56 +0300

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 const Footer = () => (
   <footer className="bg-dark text-white py-4 mt-5">
     <Container className="text-center">
-      <p>&copy; 2024 Daily Devotion. All Rights Reserved.</p>
+      <p>&copy; moabmo 2024 Daily Devotion. All Rights Reserved.</p>
     </Container>
   </footer>
 );
